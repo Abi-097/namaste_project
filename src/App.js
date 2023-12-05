a0d15a112d92ad3828b3c830e57040fa6f831277
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2023-12-05 19:56:23 +0530

    navbar creation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import Navbar from "./Components/Navbar/Index";
+// import NavbarWithMegaMenu from "./Components/Navbar/Index";
+import { NavbarWithMegaMenu } from "./Components/Navbar/Index";
+function App() {
+  return (
+    <div>
+      <NavbarWithMegaMenu />
+    </div>
+  );
+}
+
+export default App;
